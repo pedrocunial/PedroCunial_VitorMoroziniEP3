caa7f977534e6767de273fac9704743f2fbb87c2
vitormorozini <vitor_morozini@hotmail.com>
2015-04-22 08:05:58 -0300

    V12
    Graphics

--- ep3.py
+++ ep3.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 18:19:52 2015
+
+@author: vitor_000
+"""
 
 #Pedro Cunial & Vitor Morozini - 1A
 
 import datetime
-import tkinter.simpledialog as tk
-import tkinter
-root = tkinter.Tk()
-root.withdraw()
+import matplotlib.pyplot	as plt
+
 
 
 limpa = []
@@ -27,16 +30,16 @@ print(limpa[2][0][0])
 #ENCONTREI O ERRO, ISSO NÃO É UMA MATRIZ COM VÁRIOS VALORES E SIM UMA MATRIZ COM UM VALOR SEPARANDO COM ';'
 
 #ARRUMEI O PROBLEMA
-
-dic = {}
-dic[0] = limpa[0]
-for i in range(1,len(limpa)):
-    for j in range(len(limpa[i])):
-        if j == 1:
-            dic[limpa[i][0]] = limpa[i][j]
-        elif j > 1:
-            dic[limpa[i][0]] += limpa[i][j]
-print(dic)
+#
+#dic = {}
+#dic[0] = limpa[0]
+#for i in range(1,len(limpa)):
+#    for j in range(len(limpa[i])):
+#        if j == 1:
+#            dic[limpa[i][0]] = limpa[i][j]
+#        elif j > 1:
+#            dic[limpa[i][0]] += limpa[i][j]
+#print(dic)
 
 nome = str(input('Qual o seu nome?\n'))
 idade = int(input('Quantos anos voce tem?\n'))
@@ -50,6 +53,7 @@ print('Ola ', nome,  ', voce tem ', idade, 'e mede ', altura, 'm, voce eh ', sex
 
 run = True
 meses = {'janeiro':1, 'fevereiro':2, 'marco':3, 'abril':4, 'maio':5, 'junho':6, 'julho':7, 'agosto':8, 'setembro':9, 'outubro':10, 'novembro':11, 'dezembro':12}
+datas = []
 
 while run == True:
     dia = int(input('Qual o dia referente ao alimento comido?\n'))
@@ -64,7 +68,9 @@ while run == True:
 
     data = datetime.date(ano,mes,dia)
     print(data)
-    
+    datas.append(data)
+    if data >= datas[0] + 7:
+        run = False
     
     c = True
     while c == True:
@@ -76,6 +82,7 @@ while run == True:
                 print('valor computado')
                 cont += 1
                 print(limpa[i][0][0])
+                c = False
         if cont == 1:
             print('Encontrado')
             c = False
@@ -84,3 +91,51 @@ while run == True:
         else:
             print('Valor não presente na lista')
             print(cont)
+
+
+
+#Quantidade recomendada de calorias
+
+import matplotlib.pyplot	as plt
+
+peso = int(input('Qual o seu peso, em kg?'))
+
+dic2 = {'minimo':1 , 'baixo':2 , 'medio':3 , 'alto':4 , 'muito ativo':5}
+
+if faf == 1:
+   coef = 1.2
+
+if faf == 2:
+   coef = 1.375
+   
+if faf == 3:
+   coef = 1.55
+   
+if faf == 4:
+   coef = 1.725
+   
+if faf == 5:
+   coef = 1.9
+
+if sexo == 'm':
+    TMB = 88.36 + (13.4*peso) + (4.8*altura*100) - (5.7*idade)
+else:
+    TMB = 447.6 + (9.2*peso) + (3.1*altura*100)-(4.3*idade)
+   
+
+reccal = TMB*coef
+   
+limpa2 = []
+
+tempo = list(range(1,8))
+
+for i in range(7):
+    limpa2.append(reccal)
+ 
+plt.plot(tempo,limpa2)
+plt.axis([1,7,0,10000])
+plt.ylabel('Calorias')
+plt.xlabel('Dias da semana')
+plt.title(r'Quantidade de calorias recomendada')
+plt.show()
+ 
